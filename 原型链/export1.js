f60e6f72391111302855c80ab38b2c4eb31ccba7
ChenSimin0103 <375021462@qq.com>
2018-05-14 11:24:07 +0800

    updata 180514

--- 原型链/export1.js
+++ 原型链/export1.js
@@ -2,13 +2,13 @@
  * Created by 331095 on 2018/5/2.
  */
 
-export default {
-  // 寻找数组的最小值
-  findMin: function (nums) {
-    return Math.min(...nums)
-  }
-}
-//const findMin = function(nums) {
-//  return Math.min(...nums)
+//export default {
+//  // 寻找数组的最小值
+//  findMin: function (nums) {
+//    return Math.min(...nums)
+//  }
 //}
-//export default findMin;
+const findMin = function(nums) {
+  return Math.min(...nums)
+}
+export {findMin};
